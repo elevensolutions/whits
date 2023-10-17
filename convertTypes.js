3b98dfde90b861c01e9b11515c10d85b61786cde
Jeremy Wolfe <jeremy.wolfe@think11.com>
2023-10-17 00:12:25 -0500

    Update handling of HTML types.

--- convertTypes.js
+++ convertTypes.js
@@ -4,7 +4,7 @@ const globalTypes = htmlElementAttributes['*'];
 delete htmlElementAttributes['*'];
 console.log(`export type GlobalAttribute = ${globalTypes.map((value) => `'${value}'`).join(' | ')};`);
 
-const attrMap = Object.entries(htmlElementAttributes).map(([tag, types]) => `\t'${tag}': GlobalAttribute | ${types.map((value) => `'${value}'`).join(' | ')};`);
+const attrMap = Object.entries(htmlElementAttributes).map(([tag, types]) => `\t'${tag}': ${types.map((value) => `'${value}'`).join(' | ')};`);
 console.log(`interface AttributeMap extends Record<keyof HTMLElementTagNameMap, string> {\n${attrMap.join('\n')}\n}`);
 console.log(`export type HTMLTag = keyof AttributeMap & keyof HTMLElementTagNameMap;`)
 console.log(`export type HTMLAttribute<T extends HTMLTag> = AttributeMap[T];`);
